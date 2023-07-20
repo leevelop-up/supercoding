189201fee8b719f623289e8499b0f739434550ac
leevelop <merong0075@gmail.com>
2023-07-20 23:19:27 +0900

    추상화 클래스

---
+++ src/chapter_35/Abstract.java
@@ -0,0 +1,15 @@
+package chapter_35;
+
+public class Abstract {
+}
+/*
+추상화
+불필요한 정보들을 없애고 정말 필요한 정보들만 모아놓은 것
+
+
+oop 코드 추상화
+ - 클래스들 공통적인 속성,행위를 정의하여 코드 중복 줄이고, 다형성 실현함
+
+ 추상클래스는 인스턴스화(new customer 객체만드는동작) 할수 없다.
+ -abstract class의 abstract메소드를 만들면 extends받는 클래스에서는 무조건 override를 해야 한다.
+ */
